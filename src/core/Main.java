6b7bc8d5f676d26cc13fc32ecc57ccd60c03317c
Arturo Vergara <arturovergara@protonmail.com>
2020-11-21 15:05:22 -0300

    Se organizaron los recursos y se termino el login

--- src/core/Main.java
+++ src/core/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application
         Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
 
         primaryStage.setTitle("Sistema de Registro Renal");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show(); 
     }
